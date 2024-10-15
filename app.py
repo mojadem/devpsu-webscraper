01437d574ac1cdaa688f68a62f8f152871f02723
Matt Dembiczak <dembiczakmatt@gmail.com>
2024-10-15 18:32:43 -0400

    add update route

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import flask
 
+from database import check_for_unsaved_listings, overwrite_saved_listings
 from errors import ScrapingError
 from scrape import scrape_listings
 from utils import filter_listings
@@ -19,4 +20,19 @@ def index():
     listings = filter_listings(listings, args)
 
     res = {"listings": listings}
+    return res, 200
+
+
+@app.route("/update", methods=["POST"])
+def update():
+    try:
+        listings = scrape_listings()
+    except ScrapingError:
+        return "no data", 500
+
+    unsaved_listings = check_for_unsaved_listings(listings)
+
+    overwrite_saved_listings(listings)
+
+    res = {"newListings": unsaved_listings}
     return res, 200
